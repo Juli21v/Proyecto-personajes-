9e227f60818b64efcbe64c56ca73f20a94334137
Juli21v <julianvar212@gmail.com>
2021-09-27 16:24:43 -0500

    primer commit

---
+++ JS/java.js
@@ -0,0 +1,18 @@
+const crashskin = () => {
+    alert("Crash post-apocalíptico")
+}
+const cortexskin = () =>{
+     alert("Cortex faraón")
+}
+const cocoskin = () => {
+    alert("Coco princesa")
+}
+const nginskin = () => {
+    alert("N.gin bailarina")
+}
+const pasadenaskin = () => {
+    alert("Pasadena acrobática")
+}
+const ninaskin = () => {
+    alert("Nina Muñequita rosa")
+}
